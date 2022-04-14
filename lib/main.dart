0998e6a09be4621cbedf290100f356a9f282d0c9
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-04-14 13:54:01 +0545

    create basic i block

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final helloWorldProvider = Provider((_) => 'Hello world');
+import 'package:solarium/tetromino/i_block.dart';
 
 void main() {
-  runApp(ProviderScope(
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final value = ref.watch(helloWorldProvider);
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: TetrisBoard(),
+      home: const Scaffold(
+        body: TetrisBoard(),
+      ),
     );
   }
 }
@@ -41,8 +28,8 @@ class TetrisBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(floatingActionButton: FloatingActionButton(onPressed: () {
-      print('data');
-    }));
+    return CustomPaint(
+      painter: IBlock(),
+    );
   }
 }
