b931e2012192ce64180242f4a3019e8160c5c3f0
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-04-24 12:41:30 +0545

    add basic collision detection

--- lib/engine.dart
+++ lib/engine.dart
@@ -10,11 +10,10 @@ class Engine {
   static const int COL_COUNT = 20;
   final double boardWidth, boardHeight;
   int extent = 0, effectiveWidth = 0, effectiveHeight = 0;
-  final List<Tetrimino> _setPieces = [];
-  final List<int> _barrier = List.filled(COL_COUNT, -1);
+  final List<TetrisUnit> _setPieces = [];
 
   final StreamController<Tetrimino> _playerController = StreamController();
-  final StreamController<List<Tetrimino>> _gridController = StreamController();
+  final StreamController<List<TetrisUnit>> _gridController = StreamController();
 
   Engine({required this.boardWidth, required this.boardHeight}) {
     effectiveWidth = (boardWidth / COL_COUNT).floor() * COL_COUNT;
@@ -29,8 +28,7 @@ class Engine {
   Stream<Tetrimino> get playerStream =>
       _playerController.stream.flatMap((tetramino) {
         var _current = tetramino;
-        return RangeStream(0,
-                ((effectiveHeight ~/ extent) - getMaxExtentByPiece(_current)))
+        return RangeStream(0, ((effectiveHeight ~/ extent) - 0))
             .interval(const Duration(milliseconds: 100))
             .map(
           (offset) {
@@ -43,32 +41,20 @@ class Engine {
             );
           },
         ).takeWhile((_piece) {
-          final _nextPiece = Tetrimino(
-              current: _piece.current,
-              origin: _piece.origin,
-              position: _piece.position + 1);
-          final _nextIndexes = mapToGridIndex(_nextPiece, extent, COL_COUNT);
-          return _nextIndexes.any((item) => _barrier.contains(item));
+          final _nextIndexes = mapToGridIndex(_piece, extent, COL_COUNT);
+          return !_setPieces.any((item) => _nextIndexes.contains(item.index));
         }).doOnData((piece) {
           _current = piece;
         }).doOnDone(() {
-          _setPieces.add(_current);
-          final _currentIndexes = mapToGridIndex(_current, extent, COL_COUNT);
-
+          _setPieces.addAll(mapToGridIndex(
+                  _current, extent, boardWidth ~/ extent)
+              .map((item) => TetrisUnit(index: item, color: _current.color!)));
           _gridController.add(_setPieces);
           spawn();
         });
       });
 
-  Stream<List<TetrisUnit>> get gridStateStream =>
-      _gridController.stream.map((List<Tetrimino> setPieces) {
-        final _setIndexes = <TetrisUnit>[];
-        for (final piece in setPieces) {
-          _setIndexes.addAll(mapToGridIndex(piece, extent, boardWidth ~/ extent)
-              .map((item) => TetrisUnit(index: item, color: piece.color!)));
-        }
-        return _setIndexes;
-      });
+  Stream<List<TetrisUnit>> get gridStateStream => _gridController.stream;
 
   void spawn() {
     final _availablePieces = [
@@ -82,9 +68,11 @@ class Engine {
     ];
 
     _playerController.add(Tetrimino(
-      current: _availablePieces[1],
-      origin:
-          Point<double>(Random().nextInt(COL_COUNT - 4).toDouble() * extent, 0),
+      current: _availablePieces[5],
+      origin: Point<double>(0, 0),
+      // origin:
+      //     Point<double>(Random().nextInt(COL_COUNT - 4).toDouble() * extent,
+      // 0),
     ));
   }
 }
